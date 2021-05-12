bce71deffa05264de377f6dfc2f9b336c064d3f7
AZIZGASIMOV94 <aziz.gasimov94@gmail.com>
2021-05-12 19:27:01 +0400

    Currency icons ared added

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,4 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faEuroSign,
+  faLiraSign,
+  faPoundSign,
+  faRubleSign,
+  faShekelSign
+} from "@fortawesome/free-solid-svg-icons";
+
 
 const Home = () => {
 
@@ -25,17 +35,29 @@ const Home = () => {
 
   return (
     <div>
-      <div className="shadow-2xl border-l-r-t-b-4 text-4xl border-fuchsia-600 rounded-2xl p-3">
+      <div className="shadow-2xl border-l-r-t-b-4 text-4xl border-fuchsia-600 rounded-2xl p-5">
         {" "}
-        Base Rate: {baseCurr}{" "}
+        Base Rate: <FontAwesomeIcon icon={faEuroSign} /> {baseCurr}
       </div>
 
       <div className="pt-4 shadow-2xl border-l-r-t-b-4 text-2xl border-fuchsia-600 rounded-b-2xl p-3">
         <ul>
-          <li className="pt-2 p-3">Turkish Lira: {rates.TRY}</li>
-          <li className=" pt-2 p-3">British Pound: {rates.GBP}</li>
-          <li className="pt-2 p-3">Russian Ruble: {rates.RUB}</li>
-          <li className="pt-2 p-3">Israeli Shekel: {rates.SEK}</li>
+          <li className="pt-2 p-3">
+            <FontAwesomeIcon icon={faLiraSign} /> &nbsp; Turkish Lira:{" "}
+            {rates.TRY}
+          </li>
+          <li className=" pt-2 p-3">
+            <FontAwesomeIcon icon={faPoundSign} /> &nbsp; British Pound:{" "}
+            {rates.GBP}
+          </li>
+          <li className="pt-2 p-3">
+            <FontAwesomeIcon icon={faRubleSign} /> &nbsp; Russian Ruble:{" "}
+            {rates.RUB}
+          </li>
+          <li className="pt-2 p-3">
+            <FontAwesomeIcon icon={faShekelSign} /> &nbsp; Israeli Shekel:{" "}
+            {rates.SEK}
+          </li>
         </ul>
       </div>
     </div>
